3101dbb810aad30fac8b95e3263a191759953768
Vasyl Shtenovych <vasyl.shtenovych@gmail.com>
2021-05-14 17:51:02 +0300

    Switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_07_124254) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
