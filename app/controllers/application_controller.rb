83f6269e1943d62176e23e16c633d1b7e9bc25c5
Vasyl Shtenovych <vasyl.shtenovych@gmail.com>
2021-05-15 19:40:05 +0300

    Add username to user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  private
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+  def configure_permitted_parameters
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+  end
+
+  private
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
