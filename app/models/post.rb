2ff32272457b3995fb8a28682cbf784c21eb22e4
Vasyl Shtenovych <vasyl.shtenovych@gmail.com>
2021-05-15 18:25:35 +0300

    Change posts ordering

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
+
+  scope :ordered, ->(direction = :desc) { order(created_at: direction) }
+  scope :with_authors, -> { includes(:author) }
 end
